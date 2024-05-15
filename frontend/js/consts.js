0b42edb17718785f3884c9d28a762ac8d49910cc
yuvalpress <yuvalpress@gmail.com>
2024-05-15 19:59:34 +0300

    remove secrets

--- frontend/js/consts.js
+++ frontend/js/consts.js
@@ -1,6 +1,6 @@
 // Constants for Google API usage
-export const API_KEY = 'AIzaSyDF8_WLoFWIOdvMg4o8XkAU8OvOFw0QlBw';
-export const CLIENT_ID = '548727952845';
+export const API_KEY = 'xxxxx';
+export const CLIENT_ID = 'xxxxx';
 export const DISCOVERY_DOCS = ["https://www.googleapis.com/discovery/v1/apis/drive/v3/rest"];
 export const SCOPES = 'https://www.googleapis.com/auth/drive.metadata.readonly';
 
