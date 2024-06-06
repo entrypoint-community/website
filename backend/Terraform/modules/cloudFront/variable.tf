437bbea6f83ec806e94dbe9bdcfd1e28b0e8f7ad
snirkap <snirwork1@gmail.com>
2024-06-06 19:57:28 +0300

    Separation of each module and modify the providers

--- backend/Terraform/modules/cloudFront/variable.tf
+++ backend/Terraform/modules/cloudFront/variable.tf
@@ -1,9 +1,3 @@
-variable "s3_bucket_name" {
-  type        = string
-  description = "the name of the bucket"
-  default     = "website_entryPoint"
-}
-
 variable "s3_bucket_website_index_document" {
   type        = string
   description = "the default file for the static website"
@@ -26,16 +20,4 @@ variable "aws_cloudfront_distribution_acm_certificate_arn" {
   type        = string
   description = "acm certificate arn"
   default     = "arn:aws:acm:us-east-1::certificate/" #change for the real acm certificate arn forom the AWS Certificate Manager
-}
-
-variable "route53_domain_name" {
-  description = "The domain name for the hosted zone"
-  type        = string
-  default     = "www.entrypoint" # change to the real DNS name 
-}
-
-variable "route53_record_name" {
-  type        = string
-  description = "name of the record"
-  default     = "surfsupsnir.com"
 }
