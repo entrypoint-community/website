b322566cca76e18470ed2e5f57cfe820af6bcb06
snirkap <snirwork1@gmail.com>
2024-06-10 21:33:43 +0300

    modify rest api

--- backend/Terraform/modules/management_api/rest_api_getway.tf
+++ backend/Terraform/modules/management_api/rest_api_getway.tf
@@ -1,158 +1,96 @@
 resource "aws_api_gateway_rest_api" "management_api" {
-  name        = var.api_name
+  name = var.api_name
   endpoint_configuration {
     types = ["REGIONAL"]
   }
 }
 
-resource "aws_api_gateway_resource" "announcement_lambda" {
+resource "aws_api_gateway_resource" "lambda_resource" {
+  for_each    = var.lambda_paths
   rest_api_id = aws_api_gateway_rest_api.management_api.id
   parent_id   = aws_api_gateway_rest_api.management_api.root_resource_id
-  path_part   = "announcement_lambda"
+  path_part   = each.value
 }
 
-resource "aws_api_gateway_method" "proxy" {
-  rest_api_id = aws_api_gateway_rest_api.management_api.id
-  resource_id = aws_api_gateway_resource.announcement_lambda.id
-  http_method = "POST"
+resource "aws_api_gateway_method" "lambda_method" {
+  for_each      = aws_api_gateway_resource.lambda_resource
+  rest_api_id   = aws_api_gateway_rest_api.management_api.id
+  resource_id   = each.value.id
+  http_method   = var.http_method
   authorization = "NONE"
 }
 
 resource "aws_api_gateway_integration" "lambda_integration" {
-  rest_api_id = aws_api_gateway_rest_api.management_api.id
-  resource_id = aws_api_gateway_resource.announcement_lambda.id
-  http_method = aws_api_gateway_method.proxy.http_method
-  integration_http_method = "POST"
-  type = "AWS"
-  uri = aws_lambda_function.name_of_the_lambda.invoke_arn #change to the real name of the lambda
+  for_each                = aws_api_gateway_resource.lambda_resource
+  rest_api_id             = aws_api_gateway_rest_api.management_api.id
+  resource_id             = each.value.id
+  http_method             = aws_api_gateway_method.lambda_method[each.key].http_method
+  integration_http_method = var.http_method
+  type                    = "AWS"
+  uri                     = aws_lambda_function[each.key].invoke_arn
 }
 
-resource "aws_api_gateway_method_response" "proxy" {
-  rest_api_id = aws_api_gateway_rest_api.management_api.id
-  resource_id = aws_api_gateway_resource.announcement_lambda.id
-  http_method = aws_api_gateway_method.proxy.http_method
-  status_code = "200"
-
-    //cors section
+resource "aws_api_gateway_method_response" "lambda_method_response" {
+  for_each      = aws_api_gateway_resource.lambda_resource
+  rest_api_id   = aws_api_gateway_rest_api.management_api.id
+  resource_id   = each.value.id
+  http_method   = aws_api_gateway_method.lambda_method[each.key].http_method
+  status_code   = var.method_response_status_code
   response_parameters = {
     "method.response.header.Access-Control-Allow-Headers" = true,
     "method.response.header.Access-Control-Allow-Methods" = true,
     "method.response.header.Access-Control-Allow-Origin" = true
   }
-
 }
 
-resource "aws_api_gateway_integration_response" "proxy" {
-  rest_api_id = aws_api_gateway_rest_api.management_api.id
-  resource_id = aws_api_gateway_resource.announcement_lambda.id
-  http_method = aws_api_gateway_method.proxy.http_method
-  status_code = aws_api_gateway_method_response.proxy.status_code
-
-    //cors
+resource "aws_api_gateway_integration_response" "lambda_integration_response" {
+  for_each      = aws_api_gateway_resource.lambda_resource
+  rest_api_id   = aws_api_gateway_rest_api.management_api.id
+  resource_id   = each.value.id
+  http_method   = aws_api_gateway_method.lambda_method[each.key].http_method
+  status_code   = aws_api_gateway_method_response.lambda_method_response[each.key].status_code
   response_parameters = {
-    "method.response.header.Access-Control-Allow-Headers" =  "'Content-Type,X-Amz-Date,Authorization,X-Api-Key,X-Amz-Security-Token,Content-Length,DateX-Amz-Apigw-Id,X-Amzn-Errortype,X-Amzn-Requestid '",
+    "method.response.header.Access-Control-Allow-Headers" = "'Content-Type,X-Amz-Date,Authorization,X-Api-Key,X-Amz-Security-Token,Content-Length,DateX-Amz-Apigw-Id,X-Amzn-Errortype,X-Amzn-Requestid '",
     "method.response.header.Access-Control-Allow-Methods" = "'GET,OPTIONS,POST,PUT'",
     "method.response.header.Access-Control-Allow-Origin" = "'*'"
-}
-  
+  }
   depends_on = [
-    aws_api_gateway_method.proxy,
     aws_api_gateway_integration.lambda_integration
   ]
 }
 
 resource "aws_api_gateway_deployment" "deployment" {
   rest_api_id = aws_api_gateway_rest_api.management_api.id
-  stage_name = var.api_deployment_name
-  
-  depends_on = [
-    aws_api_gateway_integration.lambda_integration,
-  ]
+  stage_name  = var.api_deployment_name
+  depends_on  = [aws_api_gateway_integration.lambda_integration]
 }
 
-#if you we we will move this resource for the lambda module 
 resource "aws_iam_role" "iam_for_lambda" {
   name = "lambda-role"
-
   assume_role_policy = jsonencode({
     Version = "2012-10-17",
     Statement = [
-    {
-      Action = "sts:AssumeRole",
-      Effect = "Allow",
-      Principal = {
-        Service = "lambda.amazonaws.com"
+      {
+        Action = "sts:AssumeRole",
+        Effect = "Allow",
+        Principal = {
+          Service = "lambda.amazonaws.com"
+        }
       }
-    }
-  ]
-})
+    ]
+  })
 }
 
 resource "aws_iam_role_policy_attachment" "lambda_basic" {
   policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
-  role = aws_iam_role.iam_for_lambda.name
+  role       = aws_iam_role.iam_for_lambda.name
 }
 
 resource "aws_lambda_permission" "apigw_lambda" {
+  for_each     = aws_api_gateway_resource.lambda_resource
   statement_id = "AllowExecutionFromAPIGateway"
-  action = "lambda:InvokeFunction"
-  function_name = aws_lambda_function.name_of_the_lambda.function_name #change to the real name of the lambda
-  principal = "apigateway.amazonaws.com"
-  source_arn = "${aws_api_gateway_rest_api.management_api.execution_arn}/*/*/*"
-}
-
-resource "aws_api_gateway_resource" "blog_post_lambda" {
-  rest_api_id = aws_api_gateway_rest_api.management_api.id
-  parent_id   = aws_api_gateway_rest_api.management_api.root_resource_id
-  path_part   = "blog_post_lambda"
-}
-
-resource "aws_api_gateway_method" "metric_lambda" {
-  rest_api_id = aws_api_gateway_rest_api.management_api.id
-  resource_id = aws_api_gateway_resource.blog_post_lambda.id
-  http_method = "POST"
-  authorization = "NONE"
-}
-
-resource "aws_api_gateway_integration" "metric_lambda" {
-  rest_api_id             = aws_api_gateway_rest_api.management_api.id
-  resource_id             = aws_api_gateway_resource.blog_post_lambda.id
-  http_method             = aws_api_gateway_method.metric_lambda.http_method
-  integration_http_method = "POST"
-  type                    = "AWS"
-  uri                     = aws_lambda_function.metric_lambda.invoke_arn #change to the real name of the lambda
-}
-
-resource "aws_api_gateway_method_response" "metric_lambda" {
-  rest_api_id = aws_api_gateway_rest_api.management_api.id
-  resource_id = aws_api_gateway_resource.blog_post_lambda.id
-  http_method = aws_api_gateway_method.metric_lambda.http_method
-  status_code = "200"
-
-    //cors section
-  response_parameters = {
-    "method.response.header.Access-Control-Allow-Headers" = true,
-    "method.response.header.Access-Control-Allow-Methods" = true,
-    "method.response.header.Access-Control-Allow-Origin" = true
-  }
-
-}
-
-resource "aws_api_gateway_integration_response" "metric_lambda" {
-  rest_api_id = aws_api_gateway_rest_api.management_api.id
-  resource_id = aws_api_gateway_resource.blog_post_lambda.id
-  http_method = aws_api_gateway_method.metric_lambda.http_method
-  status_code = aws_api_gateway_method_response.metric_lambda.status_code
-
-    //cors
-  response_parameters = {
-    "method.response.header.Access-Control-Allow-Headers" =  "'Content-Type,X-Amz-Date,Authorization,X-Api-Key,X-Amz-Security-Token,Content-Length,DateX-Amz-Apigw-Id,X-Amzn-Errortype,X-Amzn-Requestid '",
-    "method.response.header.Access-Control-Allow-Methods" = "'GET,OPTIONS,POST,PUT'",
-    "method.response.header.Access-Control-Allow-Origin" = "'*'"
-}
-  
-  depends_on = [
-    aws_api_gateway_method.metric_lambda,
-    aws_api_gateway_integration.metric_lambda
-  ]
+  action       = "lambda:InvokeFunction"
+  function_name = aws_lambda_function[each.key].function_name
+  principal    = "apigateway.amazonaws.com"
+  source_arn   = "${aws_api_gateway_rest_api.management_api.execution_arn}/*/*/*"
 }
--- backend/Terraform/modules/management_api/variable.tf
+++ backend/Terraform/modules/management_api/variable.tf
@@ -9,3 +9,29 @@ variable "api_deployment_name" {
     description = "the name of the api gateway deployment"
     default = "dev"
 }
+
+variable "lambda_paths" {
+  description = "Map of Lambda function paths"
+  type        = map(string)
+  default     = {
+    announcement = "announcement_lambda"
+    blog_post    = "blog_post_lambda"
+    drive_contects = "drive_contects"
+    community_statistics = "community_statistics"
+    community_members = "community_members"
+    registration_form = "registration_form"
+    community_blog_posts = "community_blog_posts"
+  }
+}
+
+variable "http_method" {
+    type = string
+    description = "http_method"
+    default = "POST"
+}
+
+variable "method_response_status_code" {
+    type = string
+    description = "method response status code"
+    default = "200"
+}
