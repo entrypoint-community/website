2c442ffc6a8e3f3c01a8c12bb1f3c63dad23fa63
liron deri <liron.deri3@outlook.com>
2024-06-11 14:06:58 +0300

    update

--- backend/lambda/lambdaRetrieveDataLibrary/lambda_return_data.py
+++ backend/lambda/lambdaRetrieveDataLibrary/lambda_return_data.py
@@ -32,7 +32,7 @@ def get_postgres_credentials(secret_Name) -> db_creds:
 
 def check_table(table):
     try:
-        credential = get_postgres_credentials(secret_Name='secretName') # replace the secret with the actual secret for the DB creds
+        credential = get_postgres_credentials(secret_Name=os.environ["secret_name"]) # replace the secret with the actual secret for the DB creds
     except json.JSONDecodeError as e:
             return {
                 'statusCode': 400,
@@ -91,76 +91,3 @@ def check_table(table):
             'body': json.dumps({"error": str(e)})
         }
 
-def lambda_handler(event, context, table):
-    try:
-        credential = get_postgres_credentials(secret_Name='secretName') # replace the secret with the actual secret for the DB creds
-    except json.JSONDecodeError as e:
-            return {
-                'statusCode': 400,
-                'headers': {
-                    'Access-Control-Allow-Origin': '*',
-                    'Content-Type': 'application/json'
-                },
-                'body': json.dumps({"error": "Invalid JSON syntax", "message": str(e)})
-            }
-    
-    # try to connect to the DB
-    try:
-        connection = psycopg2.connect(
-            user=credential.username,
-            password=credential.password,
-            host=credential.host,
-            database=credential.database,
-            connect_timeout=10
-        )
-    except OperationalError as e:
-        print(f"The error '{e}' occurred")
-        return {
-            'statusCode': 500,
-            'headers': {
-                'Access-Control-Allow-Origin': '*',
-                'Content-Type': 'application/json'
-            },
-            'body': json.dumps({"error": "Database connection failed", "message": str(e)})
-        }
-
-    try:  
-        cursor = connection.cursor()
-        
-        query = sql.SQL("SELECT * FROM {}").format(sql.Identifier(table))
-        cursor.execute(query)
-        
-        table_data = cursor.fetchall()
-        
-        cursor.close()
-        connection.close()
-        
-        return {
-            'statusCode': 200,
-            'headers': {
-                'Access-Control-Allow-Origin': '*',
-                'Content-Type': 'application/json'
-            },
-            'body': json.dumps(table_data)
-        }
-
-    except OperationalError as e:
-        print(f"The error '{e}' occurred")
-        return {
-            'statusCode': 500,
-            'headers': {
-                'Access-Control-Allow-Origin': '*',
-                'Content-Type': 'application/json'
-            },
-            'body': json.dumps({"error": str(e)})
-        }
-    except Exception as error:
-        print(f"Error occurred: {error}")
-        return {
-            'statusCode': 500,
-            'headers': {
-                'Access-Control-Allow-Origin': '*',
-                'Content-Type': 'application/json'
-            },
-            'body': json.dumps({"error": str(error)})
-        }
--- backend/lambda/lambda_return_user_info/lambda-returns-usr-info.py
+++ backend/lambda/lambda_return_user_info/lambda-returns-usr-info.py
@@ -1,13 +1,79 @@
-from lambdaRetrieveDataLibrary.lambda_return_data import lambda_handler, check_table
+from lambdaRetrieveDataLibrary.lambda_return_data import get_postgres_credentials,check_table
 import json
+import os
+import psycopg2
+from psycopg2 import OperationalError, sql
 
-secret_Name = "secretName"
-event = {}
-context = {}
-table = "users"
+def lambda_handler(event, context):
+    try:
+        credential = get_postgres_credentials(secret_Name=os.environ["secret_name"]) # replace the secret with the actual secret for the DB creds
+    except json.JSONDecodeError as e:
+            return {
+                'statusCode': 400,
+                'headers': {
+                    'Access-Control-Allow-Origin': '*',
+                    'Content-Type': 'application/json'
+                },
+                'body': json.dumps({"error": "Invalid JSON syntax", "message": str(e)})
+            }
+    
+    # try to connect to the DB
+    try:
+        connection = psycopg2.connect(
+            user=credential.username,
+            password=credential.password,
+            host=credential.host,
+            database=credential.database,
+            connect_timeout=10
+        )
+    except OperationalError as e:
+        print(f"The error '{e}' occurred")
+        return {
+            'statusCode': 500,
+            'headers': {
+                'Access-Control-Allow-Origin': '*',
+                'Content-Type': 'application/json'
+            },
+            'body': json.dumps({"error": "Database connection failed", "message": str(e)})
+        }
+    
+    try:  
+        cursor = connection.cursor()
+        
+        query = sql.SQL("SELECT * FROM users")
+        cursor.execute(query)
+        
+        table_data = cursor.fetchall()
+        
+        cursor.close()
+        connection.close()
+        
+        return {
+            'statusCode': 200,
+            'headers': {
+                'Access-Control-Allow-Origin': '*',
+                'Content-Type': 'application/json'
+            },
+            'body': json.dumps(table_data)
+        }
 
-table_exist = check_table(table)
-
-if table_exist:
-    data = lambda_handler(event, context, table)
-    data_json = json.dumps(data)
+    except OperationalError as e:
+        print(f"The error '{e}' occurred")
+        return {
+            'statusCode': 500,
+            'headers': {
+                'Access-Control-Allow-Origin': '*',
+                'Content-Type': 'application/json'
+            },
+            'body': json.dumps({"error": str(e)})
+        }
+    except Exception as error:
+        print(f"Error occurred: {error}")
+        return {
+            'statusCode': 500,
+            'headers': {
+                'Access-Control-Allow-Origin': '*',
+                'Content-Type': 'application/json'
+            },
+            'body': json.dumps({"error": str(error)})
+        }
