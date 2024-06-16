d9d4db5d3b5ae08a610c5a890630cf52eb5c3846
liron deri <liron.deri3@outlook.com>
2024-06-16 11:49:38 +0300

    update 2

--- backend/lambda/lambdaRetrieveDataLibrary/lambda_return_data.py
+++ backend/lambda/lambdaRetrieveDataLibrary/lambda_return_data.py
@@ -90,4 +90,80 @@ def check_table(table):
             },
             'body': json.dumps({"error": str(e)})
         }
+    
+def db_connect ():
+    try:
+        credential = get_postgres_credentials(secret_Name=os.environ["secret_name"]) # replace the secret with the actual secret for the DB creds
+    except json.JSONDecodeError as e:
+            return {
+                'statusCode': 400,
+                'headers': {
+                    'Access-Control-Allow-Origin': '*',
+                    'Content-Type': 'application/json'
+                },
+                'body': json.dumps({"error": "Invalid JSON syntax", "message": str(e)})
+            }
+    
+    # try to connect to the DB
+    try:
+        connection = psycopg2.connect(
+            user=credential.username,
+            password=credential.password,
+            host=credential.host,
+            database=credential.database,
+            connect_timeout=10
+        )
+        return connection
+    except OperationalError as e:
+        print(f"The error '{e}' occurred")
+        return {
+            'statusCode': 500,
+            'headers': {
+                'Access-Control-Allow-Origin': '*',
+                'Content-Type': 'application/json'
+            },
+            'body': json.dumps({"error": "Database connection failed", "message": str(e)})
+        }
 
+def retrieve_data(table_name):
+    connection = db_connect
+    try:  
+        cursor = connection.cursor()
+        
+        query = sql.SQL(f"SELECT * FROM ${table_name}")
+        cursor.execute(query)
+        
+        table_data = cursor.fetchall()
+        
+        cursor.close()
+        connection.close()
+        
+        return {
+            'statusCode': 200,
+            'headers': {
+                'Access-Control-Allow-Origin': '*',
+                'Content-Type': 'application/json'
+            },
+            'body': json.dumps(table_data)
+        }
+
+    except OperationalError as e:
+        print(f"The error '{e}' occurred")
+        return {
+            'statusCode': 500,
+            'headers': {
+                'Access-Control-Allow-Origin': '*',
+                'Content-Type': 'application/json'
+            },
+            'body': json.dumps({"error": str(e)})
+        }
+    except Exception as error:
+        print(f"Error occurred: {error}")
+        return {
+            'statusCode': 500,
+            'headers': {
+                'Access-Control-Allow-Origin': '*',
+                'Content-Type': 'application/json'
+            },
+            'body': json.dumps({"error": str(error)})
+        }
