58cbf95b724e8a5cc435f4bfebf05e7e869f2086
noa <noabt123@gmail.com>
2024-06-17 13:24:30 +0300

    add lambda module

--- backend/infrastracture_terraform/lambda_function/backend.tf
+++
@@ -1,8 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket         = "entrypoint-state-bucket"
-    key            = "lambda/terraform.tfstate"
-    region         = "us-east-1"
-    dynamodb_table = "entrypoint-table"
-  }
-}
